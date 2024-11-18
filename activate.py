3427f60aa6eeb2f64446a97e6fa037cd7f0a8856
Zemphix <vafinaaa51@gmail.com>
2024-11-18 12:16:20 +0500

    other updates

--- activate.py
+++ activate.py
@@ -2,28 +2,26 @@ from datetime import datetime
 import json
 from bot import bot
 import traceback
-from logging import DEBUG, INFO, getLogger, basicConfig, FileHandler, StreamHandler
+from logging import getLogger
+from modules import logger as l
 
-DATE_FMT = '%Y-%m-%d %H:%M:%S'
-FORMAT = '%(asctime)s : %(module)-10s : %(lineno)-4s : %(levelname)-8s : %(message)s'
-logger = getLogger()
-file = FileHandler('logs/logs.log', mode='a')
-console = StreamHandler()
-basicConfig(level=INFO, format=FORMAT, datefmt=DATE_FMT, handlers=[file, console])
+logger = getLogger('root')
 
-
-if __name__ == '__main__':    
+def main() -> None:
     try:   
         logger.info('trying to start...')
         bot.start()
     except Exception as e:
-        print('err', traceback.format_exc())
+        logger.error(traceback.format_exc())
         logger.info('bot was stopped!')
         with open('config/bot_config.json', 'r') as f:
             config = json.load(f)
             if config.get('send_notify'):
                 bot.notify.warning('Bot was stoped!')
                 bot.notify.error(f'Time: {datetime.now()}')
+
+if __name__ == '__main__':    
+    main()
 
    
         
