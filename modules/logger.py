f49895c74cfd7f82fd94019b2f7c06df41f9ec28
Zemphix <vafinaaa51@gmail.com>
2024-11-21 03:41:30 +0500

    added import debug level

--- modules/logger.py
+++ modules/logger.py
@@ -1,4 +1,4 @@
-from logging import INFO, FileHandler, StreamHandler, basicConfig, getLogger
+from logging import DEBUG, INFO, FileHandler, StreamHandler, basicConfig, getLogger
 
 DATE_FMT = '%Y-%m-%d %H:%M:%S'
 FORMAT = '%(asctime)s : %(module)-10s : %(lineno)-4s : %(levelname)-8s : %(message)s'
