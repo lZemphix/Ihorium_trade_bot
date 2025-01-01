ab9ad825210b082dde7fa3c9d837d69e97c61e67
Zemphix <vafinaaa51@gmail.com>
2025-01-01 22:09:22 +0500

    update logger

--- config/logger_config.py
+++ config/logger_config.py
@@ -1,8 +1,11 @@
-from logging import DEBUG, INFO, FileHandler, StreamHandler, basicConfig, getLogger
+from logging import DEBUG, INFO, WARNING, FileHandler, StreamHandler, basicConfig, getLogger
+import logging
 
 DATE_FMT = '%Y-%m-%d %H:%M:%S'
 FORMAT = '%(asctime)s : %(module)-10s : %(lineno)-4s : %(levelname)-8s : %(message)s'
 logger = getLogger(__name__)
-file = FileHandler('logs/logs.log', mode='a')
+file = FileHandler('logs.log', mode='a')
 console = StreamHandler()
-basicConfig(level=INFO, format=FORMAT, datefmt=DATE_FMT, handlers=[file, console])
+basicConfig(level=DEBUG, format=FORMAT, datefmt=DATE_FMT, handlers=[file, console])
+logging.getLogger('urllib3').setLevel(WARNING)
+logging.getLogger('requests').setLevel(WARNING)
